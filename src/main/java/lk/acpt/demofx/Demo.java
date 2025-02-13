eb654d5a0a049e3e1270384fe25c47e9440753c2
Pasindu Dilshan <dilshanp5400@gmail.com>
2025-02-13 11:23:24 +0530

    adUpdated 2.

--- src/main/java/lk/acpt/demofx/Demo.java
+++ src/main/java/lk/acpt/demofx/Demo.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
              public void pay() {
                  System.out.println("Payinghgyu to cujhustomer");
                  System.out.println("Payinghgyu to cujhustomer");
+                 System.out.println("Payinghgyu to cujhustomer");
 
              }
 
