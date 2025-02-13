91452a4d2292b20c9c3176f663ff7c31f5e4abe3
Pasindu Dilshan <dilshanp5400@gmail.com>
2025-02-13 11:01:36 +0530

    hi

--- src/main/java/lk/acpt/demofx/Demo.java
+++ src/main/java/lk/acpt/demofx/Demo.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
          CustomerInterface cusInterface = new CustomerInterface() {
              @Override
              public void pay() {
-                 System.out.println("Payinghgyu to customer");
+                 System.out.println("Payinghgyu to cujhustomer");
              }
 
              @Override
